1a9e8d3d09fc29d74f4da83ba06923eb1224e69a
akasiakin <45201610+akasiakin@users.noreply.github.com>
2020-04-06 13:01:39 +0300

    Prepare a script

---
+++ entrypoint.sh
@@ -0,0 +1,51 @@
+  
+#!/bin/sh
+
+set -e
+
+if [ -z "$AWS_S3_BUCKET" ]; then
+  echo "AWS_S3_BUCKET is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+  echo "AWS_ACCESS_KEY_ID is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+  echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
+  exit 1
+fi
+
+# Default to us-east-1 if AWS_REGION not set.
+if [ -z "$AWS_REGION" ]; then
+  AWS_REGION="us-east-1"
+fi
+
+# Override default AWS endpoint if user sets AWS_S3_ENDPOINT.
+if [ -n "$AWS_S3_ENDPOINT" ]; then
+  ENDPOINT_APPEND="--endpoint-url $AWS_S3_ENDPOINT"
+fi
+
+# Create a dedicated profile for this action
+aws configure --profile s3-copy-action <<-EOF > /dev/null 2>&1
+${AWS_ACCESS_KEY_ID}
+${AWS_SECRET_ACCESS_KEY}
+${AWS_REGION}
+text
+EOF
+
+# Copy all files from a folder in a given S3 bucket
+sh -c "aws s3 cp s3://${AWS_S3_BUCKET}/${AWS_S3_DIR} . --recursive \
+              --profile s3-copy-action \
+              --no-progress \
+              ${ENDPOINT_APPEND} $*"
+
+# Clear out credentials
+aws configure --profile s3-copy-action <<-EOF > /dev/null 2>&1
+null
+null
+null
+text
+EOF
